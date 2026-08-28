7b80ec727b64eaf5ea4b350f0d3ef3b01292600d
Xavi Caimel <35535998+xavikai@users.noreply.github.com>
2026-08-28 12:44:51 +0200

    Add files via upload

---
+++ assets/slidev/default-CaURI5Y7.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-0njlPT4J.js";import{_ as r}from"../index-C1zfdAfU.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
